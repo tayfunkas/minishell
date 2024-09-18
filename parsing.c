a7c21d49725b11cd4d44ca159cd87e0ac3d56271
kyla-kang <167114646+kyla-kang@users.noreply.github.com>
2024-09-18 17:37:56 +0200

    Update parsing.c: to tokenize input by "quote" or "space"

--- parsing.c
+++ parsing.c
@@ -16,7 +16,7 @@
 #include <readline/history.h>
 #include <readline/readline.h>
 
-char	*get_input(void)
+/*char	*get_input(void)
 {
 	char	*input;
 
@@ -35,22 +35,84 @@ void	parse_input(char *input)
 		printf("Token; %s\n", token);
 		token = strtok(NULL, " ");
 	}
+}*/
+
+void tokenize_input(char *input, char **args, int max_args)
+{
+	int		i;
+	int		len;
+	char	*start;
+	char	*end;
+
+	i = 0;
+	start = input;
+	while (*start && i < max_args)
+	{
+		while (ft_isspace(*start))
+			start++;
+		if (*start == '"')
+		{
+			start++;
+			end = ft_strchr(start, '"');
+		}
+		else if (*start == '\'')
+		{
+			start++;
+			end = ft_strchr(start, '\'');
+		}
+		else
+		{
+			end = start;
+			while (*end && !ft_isspace(*end))
+				end++;
+		}
+		if (end)
+		{
+			len = end - start;
+			args[i] = (char *)malloc(len + 1);
+			strncpy(args[i], start, len);
+			args[i][len] = '\0';
+			i++;
+			if (*end)
+				start = end + 1;
+			else
+				start = end;
+		}
+	}
+	args[i] = NULL;
 }
 
 int	main(void)
 {
 	char	*input;
+	char	*args[10];
+	int	i;
 
 	while(1)
 	{
-		input = get_input();
+		input = readline("minishell> ");
 		if (input == NULL)
 		{
 			printf("exit\n");
 			break;
 		}
-		if(*input)
-			parse_input(input);
+		if(input[0] != NULL)
+		{
+			add_history(input);
+			tokenize_input(input, args, 10);
+		}
+		i = 0;
+		while (args[i])
+		{
+			printf("Token %d: %s\n", i, args[i]);
+			i++;
+		}
+		i = 0;
+		while (args[i])
+		{
+			free(args[i]);
+			i++;
+		}
 		free(input);
 	}
 	rl_clear_history(); //free all the command history before terminating
