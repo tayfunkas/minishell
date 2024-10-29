c72a7985ef3518bef9a1f6549dba22fda1e9522f
kyla-kang <167114646+kyla-kang@users.noreply.github.com>
2024-10-29 21:19:31 +0100

    Added <<f function, not norminetted yet

--- source/execute/execute_command.c
+++ source/execute/execute_command.c
@@ -6,7 +6,7 @@
 /*   By: kyukang <kyukang@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 12:44:33 by tkasapog          #+#    #+#             */
-/*   Updated: 2024/10/25 19:55:12 by kyukang          ###   ########.fr       */
+/*   Updated: 2024/10/29 21:14:07 by kyukang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,43 @@ int	execute_command(t_token *start, t_token *end, t_exec_context *ctx)
 {
 	t_command	cmd;
 	int			status;
+	int			fd_in;
+
+	status = 0;
+	if (!start)
+		return (status);
+	if (ft_strcmp(start->str, "<<") == 0)
+	{
+		fd_in = STDIN_FILENO;
+		execute_redir_heredoc(start, &fd_in);
+		start = start->next->next;
+		if (start && start->type == CMD)
+		{
+			setup_command_fds(&cmd, start, end);
+			cmd.fd_in = fd_in;
+			status = execute_ext_or_int(start, end, &cmd, ctx);
+			if (cmd.fd_out != STDOUT_FILENO)
+				close(cmd.fd_out);
+		}
+		else
+			close(fd_in);
+	}
+	else if (start->type == CMD)
+	{
+		setup_command_fds(&cmd, start, end);
+		status = execute_ext_or_int(start, end, &cmd, ctx);
+		if (cmd.fd_in != STDIN_FILENO)
+			close(cmd.fd_in);
+		if (cmd.fd_out != STDOUT_FILENO)
+			close(cmd.fd_out);
+	}
+	return (status);
+}
+
+/*int	execute_command(t_token *start, t_token *end, t_exec_context *ctx)
+{
+	t_command	cmd;
+	int			status;
 
 	status = 0;
 	if (start && start->type == CMD)
@@ -35,4 +72,4 @@ int	execute_command(t_token *start, t_token *end, t_exec_context *ctx)
 			close(cmd.fd_out);
 	}
 	return (status);
-}
+}*/
