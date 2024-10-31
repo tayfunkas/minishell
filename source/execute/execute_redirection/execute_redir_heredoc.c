cddc38f851eeaa1cfc86aca923dc98360ca7c030
Tayfun <tayfun.kasapoglu@hotmail.com>
2024-10-31 18:00:42 +0100

    Update execute_redir_heredoc.c

--- source/execute/execute_redirection/execute_redir_heredoc.c
+++ source/execute/execute_redirection/execute_redir_heredoc.c
@@ -21,32 +21,37 @@ static void	errmsg_if_no_line(char *line, char *delimiter)
 	}
 }
 
-static void	heredoc_input(int *pipe_fd, char *delimiter)
+static void heredoc_input(int *pipe_fd, char **delimiters, int delimiter_count)
 {
-	char	*line;
+    char *line;
+    int current_delimiter = 0;
 
-	signal(SIGINT, child_sigint_handler);
-	close(pipe_fd[0]);
-	while (1)
-	{
-		line = readline("> ");
-		if (!line || ft_strcmp(line, delimiter) == 0)
-		{
-			errmsg_if_no_line(line, delimiter);
-			free(line);
-			break ;
-		}
-		if (write(pipe_fd[1], line, ft_strlen(line)) == -1
-			|| write(pipe_fd[1], "\n", 1) == -1)
-		{
-			perror("write to pipe");
-			free(line);
-			exit(EXIT_FAILURE);
-		}
-		free(line);
-	}
-	close(pipe_fd[1]);
-	exit(EXIT_SUCCESS);
+    signal(SIGINT, child_sigint_handler);
+    close(pipe_fd[0]);
+    while (current_delimiter < delimiter_count)
+    {
+        while (1)
+        {
+            line = readline("> ");
+            if (!line || ft_strcmp(line, delimiters[current_delimiter]) == 0)
+            {
+                errmsg_if_no_line(line, delimiters[current_delimiter]);
+                free(line);
+                break;
+            }
+            if (write(pipe_fd[1], line, ft_strlen(line)) == -1
+                || write(pipe_fd[1], "\n", 1) == -1)
+            {
+                perror("write to pipe");
+                free(line);
+                exit(EXIT_FAILURE);
+            }
+            free(line);
+        }
+        current_delimiter++;
+    }
+    close(pipe_fd[1]);
+    exit(EXIT_SUCCESS);
 }
 
 static void	heredoc_wait(int *pipe_fd, int *fd_in, pid_t pid)
@@ -66,28 +71,43 @@ static void	heredoc_wait(int *pipe_fd, int *fd_in, pid_t pid)
 		g_signal = 128 + WTERMSIG(status);
 }
 
-void	execute_redir_heredoc(t_token *current, int *fd_in)
+static void	recursive_heredoc(t_token *current, int *fd_in, char **delimiters, int *delimiter_count)
 {
-	int		pipe_fd[2];
-	char	*delimiter;
-	pid_t	pid;
+    if (current == NULL || current->type != HEREDOC)
+        return;
 
-	delimiter = current->next->str;
-	if (pipe(pipe_fd) == -1)
-	{
-		perror("pipe");
-		exit(EXIT_FAILURE);
-	}
-	pid = fork();
-	if (pid == -1)
-	{
-		perror("fork");
-		close(pipe_fd[0]);
-		close(pipe_fd[1]);
-		exit(EXIT_FAILURE);
-	}
-	if (pid == 0)
-		heredoc_input(pipe_fd, delimiter);
-	else
-		heredoc_wait(pipe_fd, fd_in, pid);
+    delimiters[*delimiter_count] = current->next->str;
+    (*delimiter_count)++;
+
+    recursive_heredoc(current->next->next, fd_in, delimiters, delimiter_count);
+}
+
+void execute_redir_heredoc(t_token *current, int *fd_in)
+{
+    int pipe_fd[2];
+    char *delimiters[100];
+    int delimiter_count = 0;
+    pid_t pid;
+
+    recursive_heredoc(current, fd_in, delimiters, &delimiter_count);
+
+    if (pipe(pipe_fd) == -1)
+    {
+        perror("pipe");
+        exit(EXIT_FAILURE);
+    }
+
+    pid = fork();
+    if (pid == -1)
+    {
+        perror("fork");
+        close(pipe_fd[0]);
+        close(pipe_fd[1]);
+        exit(EXIT_FAILURE);
+    }
+
+    if (pid == 0)
+        heredoc_input(pipe_fd, delimiters, delimiter_count);
+    else
+        heredoc_wait(pipe_fd, fd_in, pid);
 }
